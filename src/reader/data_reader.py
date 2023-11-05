fc819aee9fe48fa637493c9f2761d54351ae60fe
Javier Godon <javier.godon@gmail.com>
2023-11-05 21:59:34 +0100

    video_producer fixed

--- src/reader/data_reader.py
+++ src/reader/data_reader.py
@@ -1,22 +1,18 @@
 from definitions import ROOT_DIR
 from src.reader.video_data import VideoData
-from src.videoproducer import VideoProducer
+from src.videoproducer import video_producer
 
 
 def read_file_by_chunks(sha_id: str, element_id: str, element_alias: str, unix_epoch: int):
     buffer = 400000
     chunk = 0
     file = open(f'{ROOT_DIR}/resources/80MbSample.webm', "rb")
-    video_producer = VideoProducer()
 
     read = file.read(buffer)
     while read:
-        video_producer.produce(VideoData(sha_id=sha_id, element_id=element_id, element_alias=element_alias,
-                                         unix_epoch=unix_epoch, chunk_index=chunk, data=str(read)))
+        video_producer(VideoData(sha_id=sha_id, element_id=element_id, element_alias=element_alias,
+                                 unix_epoch=unix_epoch, chunk_index=chunk, data=str(read)))
 
         read = file.read(buffer)
 
         chunk += 1
-
-# def send(video_data: VideoData):
-#     print(f'{video_data}')
--- src/videoproducer.py
+++ src/videoproducer.py
@@ -7,18 +7,15 @@ from configuration.configuration import get_data
 from src.reader.video_data import VideoData
 
 
-class VideoProducer:
-    def __init__(self):
-        self.producer = KafkaProducer(bootstrap_servers=get_data()['kafka']['producer']['bootstrap-servers'],
-                                      # key_serializer=str.encode('utf-8'),
-                                      value_serializer=lambda x: json.dumps(x).encode('utf-8'),
-                                      acks=get_data()['kafka']['producer']['acks'],
-                                      compression_type=get_data()['kafka']['producer']['compression-type']
-                                      )
-
-    def produce(self, data: VideoData):
-        data_as_dictionary = dataclasses.asdict(data)
-        data_as_json = json.dumps(data_as_dictionary)
-        json_as_bytes = data_as_json.encode('utf-8')
-        self.producer.send(topic=get_data()['kafka']['topic'], value=data_as_dictionary)
-
+def video_producer(data: VideoData):
+    producer = KafkaProducer(bootstrap_servers=get_data()['kafka']['producer']['bootstrap-servers'],
+                             # key_serializer=str.encode('utf-8'),
+                             value_serializer=lambda x: json.dumps(x).encode('utf-8'),
+                             acks=get_data()['kafka']['producer']['acks'],
+                             compression_type=get_data()['kafka']['producer']['compression-type']
+                             )
+    data_as_dictionary = dataclasses.asdict(data)
+    data_as_json = json.dumps(data_as_dictionary)
+    json_as_bytes = data_as_json.encode('utf-8')
+    print(f'sending message: {data.chunk_index}')
+    producer.send(topic=get_data()['kafka']['topic'], value=data_as_dictionary)
